c0d4e1f972fc30063a8d7b8f1e9ba0b8e4cc7823
Marcelo Gadotti <eng.marcelogadotti@gmail.com>
2024-10-31 09:07:20 -0300

    testing Docusaurus
    Signed-off-by: Marcelo Gadotti <eng.marcelogadotti@gmail.com>

---
+++ assets/js/a2616856.6b41e6df.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[3666],{4148:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/mrgadotti.github.io/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
